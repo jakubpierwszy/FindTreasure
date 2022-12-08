db0864421a6ac2f62fec2824eff1d20c4b111d42
Jakub <jakubledzionlg@gmail.com>
2022-12-08 14:43:17 +0100

    Print and fill board

--- src/main/java/print_map/CreateMap.java
+++ src/main/java/print_map/CreateMap.java
@@ -1,4 +1,50 @@
 package print_map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CreateMap {
+
+    public static void main(String[] args) {
+        CreateMap createMap = new CreateMap();
+        createMap.fillBoard();
+        createMap.setStartPoint(0, 0);
+        createMap.setEndPoint(3, 3);
+        createMap.printBoard();
+
+    }
+
+    char[][] board = new char[4][4];
+
+    public char setStartPoint(int x, int y) {
+        return board[x][y] = 120;
+    }
+
+    public char setEndPoint(int x, int y) {
+        return board[x][y] = 122;
+    }
+
+    public void fillBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = 79;
+            }
+        }
+    }
+
+
+    public void printBoard() {
+        for (char[] chars : board) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(chars[j] + " | ");
+            }
+            System.out.println();
+            for (int x = 0; x < board.length; x++) {
+                System.out.print("- " + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
